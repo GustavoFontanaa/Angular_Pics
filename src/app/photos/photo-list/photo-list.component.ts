48e27daf457b8ee1958226f0750a0215ced2cf1d
Gustavo Fontana <gustavoasf77@gmail.com>
2022-05-06 17:44:47 -0300

    Add FontAwesome, add directive and more

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,9 +1,7 @@
 import { PhotoService } from './../photo/photo.service';
-import { Component, OnDestroy, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
+import { Component, OnInit } from "@angular/core";
 
+import { ActivatedRoute } from "@angular/router";
 import { Photo } from './../photo/photo';
 
 @Component({
@@ -11,12 +9,12 @@ import { Photo } from './../photo/photo';
   templateUrl: "./photo-list.component.html",
   styleUrls: ["./photo-list.component.css"],
 })
-export class PhotoListComponent implements OnInit, OnDestroy {
+export class PhotoListComponent implements OnInit {
 
 
   photos: Photo[] = [];
   filter: string = '';
-  debounce: Subject<string> = new Subject<string>();
+
   hasMore: boolean = true;
   currentPage: number = 1;
   userName: string = '';
@@ -29,19 +27,13 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data['photos'];
-    this.debounce
-    .pipe(debounceTime(300)) /* --> aplicando delay de 300 ms por meio do rxjs operators. */
-    .subscribe(filter => this.filter = filter);
-  }
-
-  ngOnDestroy(): void {            /* passa unsubscribe justamente para desinscrever-se do debounce.. */
-    this.debounce.unsubscribe();  /* Boa pratica utilizar OnDestroy para desalocar memoria */
   }
 
   load() {
     this.photoService
     .listFromUserPaginated(this.userName, ++this.currentPage)
     .subscribe(photos => {
+      this.filter = '';
       this.photos = this.photos.concat(photos);
       if(!photos.length) this.hasMore = false;
     });
